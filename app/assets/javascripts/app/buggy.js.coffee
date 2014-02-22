b9d279aba6731b2cd37e625c0c7f8b3368d589bd
julio <julio336@hotmail.com>
2014-02-22 12:48:30 -0600

    first commit

---
+++ app/assets/javascripts/app/buggy.js.coffee
@@ -0,0 +1,9 @@
+#= require_self
+#= require_tree ./routers
+
+window.App =
+	Routers: {}
+	initialize: ->
+		new App.Routers.MainRouter()
+		Backbone.history.start()
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buggy::Application.config.secret_token = 'c720ad4a074a28a3b06e04a6272effd2253f381d24c5026a9ae0eec465b6084d12c729367e922a0e6f44780a25d1774aab2bde1e644392c1c993be07aa2c7201'
